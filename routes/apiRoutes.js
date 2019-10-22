9111abb9ac62146880b071663177dd1922089a37
Jordan Ricarte <jordan@ricarte.net>
2019-10-22 12:48:48 -0500

    Got data that we need in console.log from Spotify API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,4 +35,36 @@ app.get("/api/user_data", function(req, res) {
         })
     }
 })
-}
+
+app.get("/api/search/:app", function(req, res) {
+    var Spotify = require('node-spotify-api');
+    var searchRequest =  [req.params.app]
+ 
+var spotify = new Spotify({
+  id: "5a842289d2ce46ea8f2cd2a6f4647304",
+  secret: "9eaede9f19504fd59402808674913f46"
+})
+
+spotify
+  .search({ type: 'track', query: searchRequest })
+  .then(function(response) {
+
+    // Artist name
+    console.log(JSON.stringify(response.tracks.items[0].album.artists[0].name, null, 2));
+
+    // Album name
+    console.log(JSON.stringify(response.tracks.items[0].album.name, null, 2));
+
+    // Album image
+    console.log(JSON.stringify(response.tracks.items[0].album.images[0]));
+
+    // Name of Song
+    console.log(JSON.stringify(response.tracks.items[0].name, null, 2));
+    
+    // Preview of Song
+    console.log(JSON.stringify(response.tracks.items[0].preview_url, null, 2));
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+})};
