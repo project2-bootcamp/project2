74dee101ace919ab2022895ca15fc5252e07e2a4
Jordan Ricarte <jordan@ricarte.net>
2019-10-23 11:50:33 -0500

    Finished Spotify feature

--- public/js/members.js
+++ public/js/members.js
@@ -1,26 +1,30 @@
 
+$(document).ready(function () {
 
-$(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
   });
 
-  $("#spotify-submit").on("click", function(event){
+  $("#spotify-submit").on("click", function (event) {
     event.preventDefault();
     console.log("u hit this button")
 
-   var searchTerm = $("#userSearch").val().trim();
-   console.log(searchTerm);
-   console.log($.get("/api/search/" + searchTerm))
+    var searchTerm = $("#userSearch").val().trim();
+    console.log(searchTerm);
+    // console.log($.get("/api/search/" + searchTerm))
 
-    $.get(`/api/search/${req.params.app}` + searchTerm, function(req, artistData){
-      console.log(artistData);
-      $("#artist-name").text(artistData.name);
-    }) 
+    $.get(`/api/search/${searchTerm}`)
+      .then(function (artistSongData) {
+      console.log(artistSongData);
+      $("#artist-name").text(artistSongData.name);
+      $("#artist-picture").attr("src", artistSongData.image);
+      $("#artist-followers").text(artistSongData.followers);
+      $("#song-name").text(artistSongData.songName);
+    })
   })
 
-  
+
 });
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,110 +1,90 @@
 var db = require("../models");
 var passport = require("../config/passport");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: "5a842289d2ce46ea8f2cd2a6f4647304",
+    secret: "9eaede9f19504fd59402808674913f46"
+})
 
-module.exports = function(app) {
+
+module.exports = function (app) {
     app.post("/api/login", passport.authenticate("local"), function (req, res) {
         res.json(req.user);
     });
 
 
-app.post("/api/signup", function(req, res) {
-    db.User.create ({
-        email: req.body.email,
-        password: req.body.password
+    app.post("/api/signup", function (req, res) {
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(function () {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function (err) {
+                res.status(401).json(err);
+            });
+    });
+
+    app.get("/logout", function (req, res) {
+        req.logout();
+        res.redirect("/login");
+    });
+
+    app.get("/api/user_data", function (req, res) {
+        if (!req.user) {
+            res.json({});
+        } else {
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            })
+        }
     })
-        .then(function() {
-            res.redirect(307, "/api/login");
-        })
-        .catch(function(err) {
-            res.status(401).json(err);
-        });
-});
 
-app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/login");
-});
-
-app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-        res.json({});
-    } else {
-        res.json({
-            email: req.user.email,
-            id: req.user.id
-        })
-    }
-})
-
-app.get("/api/search/:app", function(req, res) {
-    var Spotify = require('node-spotify-api');
-    var searchRequest =  [req.params.app]
- 
-var spotify = new Spotify({
-  id: "5a842289d2ce46ea8f2cd2a6f4647304",
-  secret: "9eaede9f19504fd59402808674913f46"
-})
-
-// spotify
-//   .search({ type: 'track', query: searchRequest })
-//   .then(function(response) {
-
-//     // Artist name
-//     console.log(JSON.stringify(response.tracks.items[0].album.artists[0].name, null, 2));
-
-//     // Album name
-//     console.log(JSON.stringify(response.tracks.items[0].album.name, null, 2));
-
-//     // Album image
-//     console.log(JSON.stringify(response.tracks.items[0].album.images[0]));
-
-//     // Name of Song
-//     console.log(JSON.stringify(response.tracks.items[0].name, null, 2));
-    
-//     // Preview of Song
-//     console.log(JSON.stringify(response.tracks.items[0].preview_url, null, 2));
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   })
-// })};
-
-spotify
-  .search({ type: 'artist', query: searchRequest })
-  .then(function(response) {
-
-    // Artist name
-    console.log(JSON.stringify(response.artists.items[0].name, null, 2));
-
-    // Picture
-    console.log(JSON.stringify(response.artists.items[0].images[0].url, null, 2));
-
-    // Spotify Follower Count
-    console.log(JSON.stringify(response.artists.items[0].followers.total, null, 2));
-
-
-    var artistData = {
-        name: JSON.stringify(response.artists.items[0].name),
-
-        image: JSON.stringify(response.artists.items[0].images[0].url),
-        
-        followers: JSON.stringify(response.artists.items[0].followers.total)
+    var artistSongData = {
+        name: "",
+        image: "",
+        followers: "",
+        songName: "",
+        songPreview: ""
     }
 
-    // // Album name
-    // console.log(JSON.stringify(response.tracks.items[0].album.name, null, 2));
+    app.get("/api/search/:app", function (req, res) {
+        var searchRequest = req.params.app
 
-    // // Album image
-    // console.log(JSON.stringify(response.tracks.items[0].album.images[0]));
 
-    // // Name of Song
-    // console.log(JSON.stringify(response.tracks.items[0].name, null, 2));
-    
-    // // Preview of Song
-    // console.log(JSON.stringify(response.tracks.items[0].preview_url, null, 2));
-    res.json(artistData)
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
-})};
+
+        spotify
+            .search({ type: 'artist', query: searchRequest })
+            .then(function (response) {
+
+
+                artistSongData.name = JSON.stringify(response.artists.items[0].name)
+
+                artistSongData.image = response.artists.items[0].images[0].url,
+
+                    artistSongData.followers = JSON.stringify(response.artists.items[0].followers.total)
+
+
+            }).then(
+                spotify
+                    .search({ type: 'track', query: searchRequest })
+                    .then(function (response) {
+
+
+                        artistSongData.songName = JSON.stringify(response.tracks.items[0].name),
+
+                            artistSongData.songPreview = JSON.stringify(response.tracks.items[0].preview_url)
+
+
+
+                        console.log("This is the artistSongData")
+                        console.log(artistSongData)
+
+                        res.json(artistSongData)
+                    })
+            )
+
+    })
+};
